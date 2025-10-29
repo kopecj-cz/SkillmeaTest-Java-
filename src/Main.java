a1e6871449c63f2c9d27a69dddf2d11630f2a9a0
Jakub Kopec <jakubkopec@seznam.cz>
2025-10-29 15:34:05 +0100

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         //Příkaz pro vypsání do konzole
+        /*
         System.out.println("Fly Eagles Fly");
+        */
 
+        String pozdrav = "Ahoj světe";
 
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
 
 
     }
