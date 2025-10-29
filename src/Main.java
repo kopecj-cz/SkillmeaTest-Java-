2ae98e6b763b536d798de4f05fe690645699c3c7
Jakub Kopec <jakubkopec@seznam.cz>
2025-10-29 16:32:08 +0100

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,36 @@ public class Main {
         System.out.println(pozdrav);
         */
 
+        // Kalkulačka
+        /*
+        int prvniCislo = 10;
+        int druheCislo = 5;
+
+        System.out.print("Součet: ");
+        System.out.println(prvniCislo + druheCislo);
+        System.out.print("Rozdíl: ");
+        System.out.println(prvniCislo - druheCislo);
+        System.out.print("Součin: ");
+        System.out.println(prvniCislo * druheCislo);
+        System.out.print("Podíl: ");
+        System.out.println(prvniCislo / druheCislo);
+        */
+
+        /*
+        float prvniDesCislo = 0.1f;
+        float druheDesCislo = 0.2f;
+
+        System.out.print("Součet: ");
+        System.out.println(prvniDesCislo + druheDesCislo);
+        System.out.print("Rozdíl: ");
+        System.out.println(prvniDesCislo - druheDesCislo);
+        System.out.print("Součin: ");
+        System.out.println(prvniDesCislo * druheDesCislo);
+        System.out.print("Podíl: ");
+        System.out.println(prvniDesCislo / druheDesCislo);
+
+        final float LUDOLFOVO_CISLO = 3.141592653f;  //konstanta = proměnná, která nejde poté již přepsat
+        */
 
 
 
