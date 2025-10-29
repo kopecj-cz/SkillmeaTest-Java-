8a49b4f8ed0ea68f5b4c89f429abfbadd9c95bbf
Jakub Kopec <jakubkopec@seznam.cz>
2025-10-29 15:59:40 +0100

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Příkaz pro vypsání do konzole
+        // Příkaz pro vypsání do konzole
         /*
         System.out.println("Fly Eagles Fly");
-
         */
 
+        /*
+        // Základy
         String pozdrav = "Ahoj světe";
+        String jmeno = "přeje Jakub";
 
+        System.out.println(pozdrav + " " + jmeno);
         System.out.println(pozdrav);
-        System.out.println(pozdrav);
+        */
+
+
+
+
 
 
     }
